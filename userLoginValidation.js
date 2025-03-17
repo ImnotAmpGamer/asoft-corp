15d02b35b7cd823cd899727e51aa01ddb027308d
ImnotAmpGamer <lgamonkar2010@gmail.com>
2025-03-17 20:54:12 +0530

    Created login validation JS file
    Created new JS file to validate user login at login page.
    Please refer to file "README.md" for user credentials.

---
+++ userLoginValidation.js
@@ -0,0 +1,14 @@
+function userLoginValidation(form) {
+            let user="";
+            let password="";
+            user = document.forms["userLogin"]["username"].value;
+            password = document.forms["userLogin"]["password"].value;
+            if (user.length == 0 || password.length == 0) { 
+                alert("Username and password cannot be blank.");
+            }
+            else if(user != "User0001" || password != "1324") {
+                alert("Incorrect User name password combination.");
+            }
+            else {
+                form.submit();
+            }
